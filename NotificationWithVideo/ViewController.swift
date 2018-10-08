f48c4ccb0f52741cc63a6c553933e706c73ce080
Paloma Bispo <palomabispo@macbook-pro-de-paloma.local>
2018-10-08 10:33:25 -0300

    notification with video

--- NotificationWithVideo/ViewController.swift
+++ NotificationWithVideo/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
-
+import UserNotifications
 class ViewController: UIViewController {
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UNUserNotificationCenter.current().delegate = self
+        
+        handleNotification()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func handleNotification(){
+        let content = UNMutableNotificationContent()
+        content.title = "This is a video"
+        content.body = "Wow!"
+        content.sound = UNNotificationSound.default
+        
+        if let url = Bundle.main.url(forResource: "movie",
+                                     withExtension: "mp4") {
 
+            if let attachment = try? UNNotificationAttachment(identifier:
+                UUID().uuidString, url: url, options: nil) {
+                content.attachments = [attachment]
+            }
+        }
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            print(error?.localizedDescription)
+        }
+
+    }
 
 }
 
+extension ViewController: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound, .badge])
+    }
+}
