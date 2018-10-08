b8ab9cd7625811e72b0d654621ec68e6f2c81823
Paloma Bispo <palomabispo@macbook-pro-de-paloma.local>
2018-10-08 09:16:04 -0300

    Initial Commit

---
+++ NotificationWithVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationWithVideo
+//
+//  Created by Paloma Bispo on 08/10/18.
+//  Copyright © 2018 Paloma Bispo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
